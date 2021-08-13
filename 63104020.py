21b80bc5c495acc863f01039d20f26c06923a038
tanachoad <80100548+tanachoad@users.noreply.github.com>
2021-08-13 11:25:56 +0700

    Add files via upload

---
+++ 63104020.py
@@ -0,0 +1,17 @@
+def  calcat_circle_area(radius):
+    PI = 3.14
+    area=PI*(radius*radius)
+    print("Circle radius {},area is {}".format(radius,area))
+
+def calcat_circle_circumference(radius):
+    PI = 3.14
+    circumference= 2*PI*radius
+    print("Circle {},circumference is {}".format(radius,circumference))
+
+def calculate_retangle_area(wide,length):
+    area = wide*length
+    print("Retangle area of {}X{} is {}".format(wide,length,area))
+
+def calculate_triangle_area(base,high):
+    area = 0.5*base*high
+    print("Tritangle area of {}X{} is {}".format(base,high,area))
